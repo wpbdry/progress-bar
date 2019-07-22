df29d7890f8c81690573103c26b66d19de366a92
William Dry <wpbdry@gmail.com>
2019-07-22 21:57:16 +0200

    add Loop class

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class ProgressBar:
     def __init__(self, name="Progress", char='#'):
         self.name = name
@@ -22,3 +23,32 @@ class ProgressBar:
         # TODO: Move width reading into update method
         self.width = 60
         self.update(progress=0)
+
+
+class Loop():
+    """
+    Pass __init__ a function that will be executed.
+    The first parameter passed to your function will be the counter, from 0 to 'iterations'.
+    You may specify ONE additional parameter, which will be passed as the second parameter.
+    """
+    def __init__(self, code_block, number_of_iterations=1, param=None, progress_bar_name=None, progress_bar_char=None):
+        self.code_block = code_block
+        self.iterations = number_of_iterations
+        self.function_param = param
+        if progress_bar_name:
+            self.progress_bar_name = progress_bar_name
+        else:
+            self.progress_bar_name = f'Running {code_block.__name__}'
+        if progress_bar_char:
+            self.progress_bar = ProgressBar(name = self.progress_bar_name, char=progress_bar_char)
+        else:
+            self.progress_bar = ProgressBar(name=self.progress_bar_name)
+
+    def execute(self):
+        self.progress_bar.start()
+        for i in range(0, self.iterations):
+            if self.function_param != None:
+                self.code_block(i, self.function_param)
+            else:
+                self.code_block(i)
+            self.progress_bar.update((i+1)/self.iterations)
