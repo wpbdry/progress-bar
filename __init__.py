21975b3abd1cfa25ee8c5813291ab5d80f791d1a
William Dry <wpbdry@gmail.com>
2019-07-22 21:17:57 +0200

    create ProgressBar with basic methods

--- __init__.py
+++ __init__.py
@@ -1,21 +1,24 @@
 import sys
 
 class ProgressBar:
-    def __init__(self, name="Progress"):
+    def __init__(self, name="Progress", char='#'):
         self.name = name
+        self.char = char
 
     def set_progress(self, progress):
         self.progress = progress
-        self.done = '#' * self.width * progress
-        self.to_do = ' ' * (self.width - done)
+        width_done = int(self.width * progress)
+        self.done_str = self.char * width_done
+        self.to_do_str = ' ' * (self.width - width_done)
 
     def update(self, progress=None):
-        if progress:
+        if progress != None:
             self.set_progress(progress)
-        percent = str(self.progress * 100)
-        sys.stdout.write(f'\r{name}: [{done}{to_do}] {percent}%')
+        percent = str(int(self.progress * 100))
+        ret = sys.stdout.write(f'\r{self.name}: [{self.done_str}{self.to_do_str}] {percent}%')
 
     def start(self):
         # TODO: Read terminal width
+        # TODO: Move width reading into update method
         self.width = 60
-        self.update(0)
+        self.update(progress=0)
